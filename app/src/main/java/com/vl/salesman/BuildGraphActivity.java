1e745de2f09d69c5c4aa5d6d162db630294fd14a
Exynos <sinerechka0000@gmail.com>
2023-06-06 15:05:17 +0700

    building graph

--- app/src/main/java/com/vl/salesman/BuildGraphActivity.java
+++ app/src/main/java/com/vl/salesman/BuildGraphActivity.java
@@ -1,11 +1,10 @@
 package com.vl.salesman;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.util.Pair;
 
 import com.vl.salesman.databinding.ActivityBuildGraphBinding;
 import com.vl.salesman.item.Point;
@@ -13,54 +12,50 @@ import com.vl.salesman.view.GraphSurfaceController;
 
 import org.jetbrains.annotations.NotNull;
 
-import java.util.Arrays;
 import java.util.stream.Stream;
 
 public class BuildGraphActivity extends AppCompatActivity implements View.OnClickListener, GraphSurfaceController.GraphSurfaceCallback {
     private ActivityBuildGraphBinding binding;
     private GraphSurfaceController surfaceController;
+    private boolean isPointChecked = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityBuildGraphBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        Stream.of(binding.addPoint, binding.info, binding.apply)
+        updateFloatingButtonIcon();
+        Stream.of(binding.floatingButton, binding.info, binding.apply)
                 .forEach(b -> b.setOnClickListener(this));
-        demo();
         surfaceController = new GraphSurfaceController(binding.buildGraphSurface);
         surfaceController.setCallback(this);
     }
 
     @Override
     public void onPointPlaced(Point point) {
-        binding.addPoint.setVisibility(View.VISIBLE);
+        binding.floatingButton.setVisibility(View.VISIBLE);
     }
 
     @Override
-    public void onPointChecked(Point point) {
-        // TODO
+    public void onPointChecked(Point point, boolean checked) {
+        isPointChecked = checked;
+        updateFloatingButtonIcon();
     }
 
-    private void demo() {
-        Point
-                p1 = new Point(200f, 200f),
-                p2 = new Point(200f, 400f),
-                p3 = new Point(400f, 200f),
-                p4 = new Point(400f, 400f);
-        binding.buildGraphSurface.getPoints().addAll(Arrays.asList(p1, p2, p3, p4));
-        binding.buildGraphSurface.getContacts().addAll(Arrays.asList(
-                new Pair<>(new Point[] {p1, p2}, false),
-                new Pair<>(new Point[] {p3, p4}, true)
-        ));
-    }
-
+    @SuppressLint("NonConstantResourceId")
     @Override
     public void onClick(@NotNull View view) {
         switch (view.getId()) {
-            case R.id.add_point:
-                binding.addPoint.setVisibility(View.GONE);
-                surfaceController.requestPointPlace();
+            case R.id.floating_button:
+                if (isPointChecked) {
+                    if (surfaceController.requestRemovePoint()) {
+                        isPointChecked = false;
+                        updateFloatingButtonIcon();
+                    }
+                } else {
+                    binding.floatingButton.setVisibility(View.GONE);
+                    surfaceController.requestPointPlace();
+                }
                 break;
             case R.id.info:
                 // TODO
@@ -69,5 +64,9 @@ public class BuildGraphActivity extends AppCompatActivity implements View.OnClic
                 // TODO
                 break;
         }
+    }
+
+    private void updateFloatingButtonIcon() {
+        binding.floatingButton.setImageResource(isPointChecked ? R.drawable.ic_delete : R.drawable.ic_add);
     }
 }
--- app/src/main/java/com/vl/salesman/view/GraphSurfaceController.java
+++ app/src/main/java/com/vl/salesman/view/GraphSurfaceController.java
@@ -4,25 +4,42 @@ import android.annotation.SuppressLint;
 import android.view.MotionEvent;
 import android.view.View;
 
+import androidx.core.util.Pair;
+
 import com.vl.salesman.item.Point;
 
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Arrays;
 
 @SuppressLint("ClickableViewAccessibility")
 public class GraphSurfaceController {
     private final static float POINT_CLICK_RADIUS = 30f;
 
     private final GraphSurface surfaceView;
+    private final TouchHandler touchHandler;
     private GraphSurfaceCallback listener;
-    private volatile boolean isPointPlacing = false;
+    private boolean isPointPlacing = false;
 
     public GraphSurfaceController(GraphSurface surfaceView) {
         this.surfaceView = surfaceView;
-        surfaceView.setOnTouchListener(new TouchHandler());
+        surfaceView.setOnTouchListener(touchHandler = new TouchHandler());
     }
 
     public void requestPointPlace() {
         isPointPlacing = true;
+    }
+
+    public boolean requestRemovePoint() {
+        if (touchHandler.isMotionHandling || touchHandler.clickPoint == null)
+            return false;
+        surfaceView.getPoints().remove(touchHandler.clickPoint);
+        surfaceView.getContacts().stream()
+                .filter(pair -> Arrays.stream(pair.first).anyMatch(p -> p == touchHandler.clickPoint))
+                .forEach(surfaceView.getContacts()::remove);
+        touchHandler.clickPoint = null;
+        return true;
     }
 
     public void setCallback(GraphSurfaceCallback listener) {
@@ -34,21 +51,27 @@ public class GraphSurfaceController {
          * Called after new point was added or existing has moved
          */
         void onPointPlaced(Point point);
-        void onPointChecked(Point point);
+        void onPointChecked(Point point, boolean checked);
     }
 
     private class TouchHandler implements View.OnTouchListener {
+        private boolean isMotionHandling = false;
         private Point clickPoint; // current "checked" point
+        private Pair<Point[], Boolean> clickConnection; // current dragging connection
 
         @Override
         public boolean onTouch(View view, MotionEvent event) {
             switch (event.getAction()) {
                 case MotionEvent.ACTION_DOWN:
+                    isMotionHandling = true;
+
+                    if (clickPoint != null && listener != null)
+                        listener.onPointChecked(clickPoint, false);
+
                     if (isPointPlacing) {
                         if (clickPoint != null)
                             clickPoint.setChecked(false);
-                        clickPoint = new Point(event.getX(), event.getY());
-                        clickPoint.setChecked(true);
+                        clickPoint = pointOf(event, true);
                         surfaceView.getPoints().add(clickPoint);
                     } else if (clickPoint != null) {
                         if (distance(event, clickPoint) > POINT_CLICK_RADIUS) {
@@ -56,23 +79,27 @@ public class GraphSurfaceController {
                             clickPoint = null;
                         } else isPointPlacing = true;
                     }
-                    if (clickPoint == null) {
-                        clickPoint = surfaceView.getPoints().stream().filter(
-                                p -> distance(event, p) <= POINT_CLICK_RADIUS
-                        ).min(
-                                (p1, p2) -> Float.compare(distance(event, p1), distance(event, p2))
-                        ).orElse(null);
-                    }
+                    if (clickPoint == null)
+                        clickPoint = findAt(event);
                     break;
 
                 case MotionEvent.ACTION_MOVE:
                     if (isPointPlacing)
                         clickPoint.set(event.getX(), event.getY());
-                    else if (clickPoint != null && distance(event, clickPoint) > POINT_CLICK_RADIUS)
+                    else if (clickPoint != null && distance(event, clickPoint) > POINT_CLICK_RADIUS) {
+                        clickPoint.setChecked(true);
+                        clickConnection = new Pair<>(new Point[]{clickPoint, pointOf(event, false)}, true);
                         clickPoint = null;
+                        surfaceView.getContacts().add(clickConnection);
+                    } else if (clickConnection != null) {
+                        Point pointTo = findAt(event);
+                        clickConnection.first[1].set(pointTo == null ? pointOf(event, false) : pointTo);
+                    }
                     break;
 
                 case MotionEvent.ACTION_UP:
+                    isMotionHandling = false;
+
                     if (isPointPlacing) {
                         isPointPlacing = false;
                         clickPoint.setChecked(false);
@@ -82,7 +109,17 @@ public class GraphSurfaceController {
                     } else if (clickPoint != null) {
                         clickPoint.setChecked(true);
                         if (listener != null)
-                            listener.onPointChecked(clickPoint);
+                            listener.onPointChecked(clickPoint, true);
+                    } else if (clickConnection != null) {
+                        Point
+                                pointFrom = clickConnection.first[0],
+                                pointTo = findAt(event);
+                        pointFrom.setChecked(false);
+                        surfaceView.getContacts().remove(clickConnection);
+                        clickConnection = new Pair<>(new Point[]{pointFrom, pointTo}, false);
+                        if (pointTo != null && pointFrom != pointTo && !isThereConnection(clickConnection))
+                            surfaceView.getContacts().add(clickConnection);
+                        clickConnection = null;
                     }
                     break;
             }
@@ -95,6 +132,29 @@ public class GraphSurfaceController {
 
         private float distance(@NotNull MotionEvent event, @NotNull Point point) {
             return distance(event.getX(), event.getY(), point.x, point.y);
+        }
+
+        @Nullable
+        private Point findAt(MotionEvent event) {
+            return surfaceView.getPoints().stream().filter(
+                    p -> distance(event, p) <= POINT_CLICK_RADIUS
+            ).min(
+                    (p1, p2) -> Float.compare(distance(event, p1), distance(event, p2))
+            ).orElse(null);
+        }
+
+        private Point pointOf(MotionEvent event, boolean checked) {
+            Point point = new Point(event.getX(), event.getY());
+            point.setChecked(checked);
+            return point;
+        }
+
+        private boolean isThereConnection(Pair<Point[], Boolean> connection) {
+            return surfaceView.getContacts().stream().map(pair -> pair.first).anyMatch(
+                    points -> Arrays.stream(points)
+                            .filter(p -> p == connection.first[0] || p == connection.first[1])
+                            .count() == 2
+            );
         }
     }
 }
